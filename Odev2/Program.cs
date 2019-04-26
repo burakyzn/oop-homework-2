230e3a7d129ce952a0d264c562a94685d07b838b
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-04-26 22:56:12 +0300

    Update Program.cs

--- Odev2/Program.cs
+++ Odev2/Program.cs
@@ -7,62 +7,12 @@ namespace Odev2
     {
         static void Main(string[] args)
         {
+            string[] futbolcular = { "Cahit Arf", "Wilhelm Rontgen", "Thomas Edison", "Michael Faraday",
+                                    "Werner Heisenberg", "Aziz Sancar", "Ali Kuþçu", "Albert Einstein",
+                                    "Nikola Tesla", "Isaac Newton"};
 
-            List<Futbolcu> takim = new List<Futbolcu>();
-
-            takim.Add(new Kaleci("Cahit Arf", 1));
-            takim.Add(new Defans("Wilhelm Rontgen", 2));
-            takim.Add(new Defans("Thomas Edison", 3));
-            takim.Add(new Defans("Michael Faraday", 4));
-            takim.Add(new Defans("Werner Heisenberg", 5));
-            takim.Add(new OrtaSaha("Aziz Sancar", 6));
-            takim.Add(new OrtaSaha("Ali Kuþçu", 7));
-            takim.Add(new OrtaSaha("Albert Einstein", 8));
-            takim.Add(new Forvet("Nikola Tesla", 9));
-            takim.Add(new Forvet("Isaac Newton", 10));
-
-            int formaNo = 0;
-            int pasAtanFormaNo;
-            bool macKazanildimi = false;
-            Random rastgeleFormaNo = new Random();
-            for (int i = 0; i < 3; i++)
-            {
-                pasAtanFormaNo = formaNo;
-
-                do
-                    formaNo = rastgeleFormaNo.Next(0, 10);
-                while (pasAtanFormaNo == formaNo);
-
-                if (i == 2)
-                {
-                    if (takim[formaNo].GolVurusu())
-                    {
-                        Console.WriteLine("GOOOOOLLLL GOOOOOLLL " + takim[formaNo].AdSoyad + "\'dan muhtesem bir gol!");
-                        macKazanildimi = true;
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine(takim[formaNo].AdSoyad + "\'in gol denemesi basarisiz top kaleyi bulmadi.");
-                        break;
-                    }
-                }
-
-                if (takim[formaNo].PasVer())
-                    Console.WriteLine(takim[formaNo].AdSoyad + "\'in pas denemesi basarili.");
-                else
-                {
-                    Console.WriteLine(takim[formaNo].AdSoyad + "\'in pas denemesi basarisiz top karsi takima gecti.");
-                    break;
-                }
-
-            }
-
-            if (macKazanildimi)
-                Console.WriteLine("Maç 1-0 skor ile kazanildi.");
-            else
-                Console.WriteLine("Maç Kaybedildi.");
-
+            Mac dostlukMacý = new Mac(futbolcular);
+            dostlukMacý.maciOynat();
         }
     }
 }
